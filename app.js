b7c91c6047f594717b959fcd33b6631e5084b026
Abelardo <37087157+avillalobosd@users.noreply.github.com>
2021-10-14 15:28:32 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const ftpClient = require("ftp-client");
+
+// FTP Configuration
+const config = {
+  host: 'ftp.abecode.com',
+  port: 21,
+  user: 'cameraroll@pruebas.abecode.com',
+  password: 'Warewolf15'
+};
+
+const options = {
+  logging: 'basic'
+};
+client = new ftpClient(config, options);
+
+client.connect(function (result) {
+  client.upload(['./upload/**'], '/empleados', {
+    baseDir: 'empleados',
+    overwrite: 'older'
+  }, function (result) {
+    console.log(result);
+  })
+}
+);
